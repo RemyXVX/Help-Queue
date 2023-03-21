769756de73202f3b7a8defb6b8ea3b107807192b
RemyXVX <remyfranciscoflores@gmail.com>
2023-03-20 19:29:23 -0700

    refactor and add button to switch between components

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -11,24 +11,27 @@ class TicketControl extends React.Component {
   }
 
   handleClick = () => {
-    this.setState({formVisibleOnPage: true});
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render() {
     let currentlyVisibleState = null;
-    let addTicketButton = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
+      buttonText="Ticket List"
     } else {
-      currentlyVisibleState = <TicketControl />
-      addTicketButton= <button onClick={this.handleClick}>Add Ticket</button>
+      currentlyVisibleState = <TicketList />
+      buttonText="Add Ticket"
     }
     // this will choose between what can be seen or not
 
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        {addTicketButton}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
